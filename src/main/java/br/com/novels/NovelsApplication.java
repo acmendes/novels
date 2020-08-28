c27c6bfa23ec5125e86d2dc149718f4b5542945b
Alexandre <alex.cep@gmail.com>
2020-08-28 02:46:06 -0300

    Adicionado o login via Github para testes.

---
+++ src/main/java/br/com/novels/NovelsApplication.java
@@ -0,0 +1,35 @@
+package br.com.novels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@SpringBootApplication
+public class NovelsApplication extends WebSecurityConfigurerAdapter  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelsApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(a -> a
+				.antMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+				)
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				)
+				.oauth2Login();
+	}
+}
